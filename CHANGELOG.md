303121e2552867916d96f8f099ab6d6c5832f7b8
Jan Vykopal <98724@mail.muni.cz>
2021-06-21 21:26:15 +0200

    First version for VBox and QEMU

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [qemu-0.1.0], [vbox-0.1.0] - 2023-06-26
+### Added
+- First version
+- VBox with python3-pip and ansible-core 2.14 and ansible 7.7.0 installed
+- QEMU with python3-pip installed
+
+[qemu-0.1.0]: https://gitlab.ics.muni.cz/muni-kypo-images/ubuntu-22.04/-/tree/qemu-0.1.0
+[vbox-0.1.0]: https://gitlab.ics.muni.cz/muni-kypo-images/ubuntu-22.04/-/tree/vbox-0.1.0
---
+++ scripts/fixes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+# disable root login using password
+sudo passwd -l root
+sudo sed -i 's/PermitRootLogin.*/PermitRootLogin no/g' /etc/ssh/sshd_config
+
+# disable upgrade popup and unattended upgrades
+sudo sed -i 's/Prompt=.*/Prompt=never/' /etc/update-manager/release-upgrades
+sudo apt-get remove update-notifier unattended-upgrades -y --purge
+
+# disable apt-daily
+sudo systemctl stop apt-daily.timer;
+sudo systemctl stop apt-daily-upgrade.timer;
+sudo systemctl disable apt-daily.timer;
+sudo systemctl disable apt-daily-upgrade.timer;
+sudo systemctl daemon-reload;
+
+# cleanup
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
+sudo rm -rf /var/log
+history -c
