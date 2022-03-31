4db97ad3bad1d6a1f41af3666d303e67a8af990f
Jan Vykopal <vykopal@ics.muni.cz>
2022-03-31 18:00:04 +0200

    Install git and CA certificates

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,7 +4,7 @@
 
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 DEBIAN_FRONTEND=noninteractive sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y cloud-init qemu-guest-agent cloud-guest-utils cloud-initramfs-growroot spice-vdagent
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y cloud-init qemu-guest-agent cloud-guest-utils cloud-initramfs-growroot spice-vdagent git apt-transport-https ca-certificates
 
 # copy custom cloud-init configuration (do not modify default apt source list)
 sudo cp -f /tmp/cloud.cfg /etc/cloud/cloud.cfg
